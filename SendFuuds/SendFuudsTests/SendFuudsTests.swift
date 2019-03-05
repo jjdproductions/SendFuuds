5ee67e786fc6e3427941ef76c6682b697aa991c4
Joshua Tavassolikhah <joshtavassolikhah@yahoo.com>
2019-03-04 22:41:58 -0800

    added project folder

---
+++ SendFuuds/SendFuudsTests/SendFuudsTests.swift
@@ -0,0 +1,34 @@
+//
+//  SendFuudsTests.swift
+//  SendFuudsTests
+//
+//  Created by Josh Tav on 3/4/19.
+//  Copyright © 2019 JoshTav. All rights reserved.
+//
+
+import XCTest
+@testable import SendFuuds
+
+class SendFuudsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

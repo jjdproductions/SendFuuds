4b53635e94c38e66b4dfea2bc6237c05dcbd518d
Justin Leong <justinleong360@gmail.com>
2019-03-07 13:57:06 -0800

    organized view controllers

---
+++ SendFuuds/SendFuuds/View Controllers/SearchViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SearchViewController.swift
+//  SendFuuds
+//
+//  Created by Justin Leong on 3/7/19.
+//  Copyright © 2019 JoshTav. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onLogout(_ sender: Any) {
+        PFUser.logOut()
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        
+        let delegate = UIApplication.shared.delegate as! AppDelegate
+        
+        delegate.window?.rootViewController = loginViewController
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
